23850e155b073360dd81c6a9a75895c736dd9af9
DargonBoi <theodore.p.maloney@gmail.com>
2023-05-12 09:57:48 -0500

    Initial Commit

---
+++ src/main.rs
@@ -0,0 +1,31 @@
+#![no_std]
+#![no_main]
+#![feature(custom_test_frameworks)]
+#![test_runner(ipp_os::test_runner)]
+#![reexport_test_harness_main = "test_main"]
+
+use core::panic::PanicInfo;
+use ipp_os::println;
+
+#[no_mangle]
+pub extern "C" fn _start() -> ! {
+    println!("Hello, world");
+
+    #[cfg(test)]
+    test_main();
+
+    loop {}
+}
+
+#[cfg(not(test))]
+#[panic_handler]
+fn panic(info: &PanicInfo) -> ! {
+    println!("{}", info);
+    loop {}
+}
+
+#[cfg(test)]
+#[panic_handler]
+fn panic(info: &PanicInfo) -> ! {
+    ipp_os::test_panic_handler(info)
+}
